d8c2ae35f95e186ca9c0f47664d8647a3164704d
Luu Tu <luutu@MacBook-Pro-cua-Luu.local>
2020-03-25 12:14:06 +0700

    Updates

---
+++ precache-manifest.600890fc480ac69853c9deed3742087d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd442950fca666582ca5f6bd03bd7059",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "84336fa9b53719909f91",
+    "url": "/demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c8b881286f93c4afab33",
+    "url": "/demo/static/js/2.0e48ec02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo/static/js/2.0e48ec02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84336fa9b53719909f91",
+    "url": "/demo/static/js/main.c37156ba.chunk.js"
+  },
+  {
+    "revision": "70116ee023940404078b",
+    "url": "/demo/static/js/runtime-main.fc0458c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
